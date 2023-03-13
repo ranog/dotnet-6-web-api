5e390f8cb0249e117cb5eea7ec7c0d03322030ed
ranog <jprnogueira@yahoo.com.br>
2023-03-12 21:18:59 -0300

    Section 9: Entity Framework Core

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>();
+
 var app = builder.Build();
 var configuration = app.Configuration;
 ProductRepository.Init(configuration);
@@ -74,7 +77,41 @@ public static class ProductRepository {
 }
 
 
+public class Category{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+
+public class Tag{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ProductId { get; set; }
+}
+
+
 public class Product{
+    public int Id { get; set; }
     public string Code { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public List<Tag> Tags { get; set; }
+}
+
+
+public class ApplicationDbContext: DbContext{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder){
+        builder.Entity<Product>().Property(p => p.Code).HasMaxLength(20).IsRequired();
+        builder.Entity<Product>().Property(p => p.Name).HasMaxLength(120).IsRequired();
+        builder.Entity<Product>().Property(p => p.Description).HasMaxLength(500).IsRequired(false);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(
+            "Server=localhost;Database=Products;User Id=sa;Password=@Sql2019;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES"
+        );
 }
